0a5d403f209d2d28e62ec9a968a76ca17097f877
leowll <leowll.kit@gmail.com>
2016-07-31 15:59:17 +0900

    add sql scripts

---
+++ web/WEB-INF/sql/add seq to shop_id.sql
@@ -0,0 +1,3 @@
+﻿create sequence shop_id_seq;
+alter table shop alter shop_id set default nextval('shop_id_seq');
+select setval('shop_id_seq', 100001 ); --set to the highest current value of shopID
---
+++ web/WEB-INF/sql/create_shop.sql
@@ -0,0 +1,7 @@
+﻿create table shop(
+ shop_id int,
+ shop_name varchar(50),
+ shop_address1 varchar(50),
+ shop_address2 varchar(50),
+ shop_url varchar(100)
+ );
